4e3c81f24c8c0d4f787397250018cd7bef038fc5
nnishant776 <nn54565852@gmail.com>
2024-03-23 19:49:43 +0000

    feat(stack): Add option to capture stack trace on creation

--- stack_opts.go
+++ stack_opts.go
@@ -5,10 +5,17 @@ type StackErrOption interface {
 }
 
 type stackErrOpts struct {
+	autoStacktrace bool
 }
 
 type stackErrOptFunc func(*stackErrOpts)
 
 func (self stackErrOptFunc) apply(o *stackErrOpts) {
 	self(o)
+}
+
+func WithTraceback() StackErrOption {
+	return stackErrOptFunc(func(o *stackErrOpts) {
+		o.autoStacktrace = true
+	})
 }
