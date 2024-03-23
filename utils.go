6fa96b8f4517495a2ae5c3f1b6532019e59516d6
nnishant776 <nn54565852@gmail.com>
2024-03-23 19:35:57 +0000

    feat(utils): Add utility to capture call stacks

--- utils.go
+++ utils.go
@@ -19,3 +19,40 @@ func caller(skip int) Frame {
 		Line:     int32(f.Line),
 	}
 }
+
+func callers(skip int, cnt int) []Frame {
+	var frames []Frame
+
+	if cnt < 0 {
+		return frames
+	}
+
+	var pcs = make([]uintptr, 20)
+	if cnt > 0 && cnt < 20 {
+		pcs = pcs[:cnt]
+	}
+
+	var count = runtime.Callers(skip+1, pcs[:])
+	frames = make([]Frame, 0, count)
+
+	if count == 0 {
+		return frames
+	}
+
+	var callFrames = runtime.CallersFrames(pcs[:count])
+
+	for {
+		f, ok := callFrames.Next()
+		frames = append(frames, Frame{
+			File:     f.File,
+			Function: f.Function,
+			Line:     int32(f.Line),
+		})
+
+		if !ok {
+			break
+		}
+	}
+
+	return frames
+}
