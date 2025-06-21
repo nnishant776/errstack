a27eb6cf601788e9ae8b03fa48b33d140d3927db
nnishant776 <nn54565852@gmail.com>
2025-06-21 14:31:39 +0000

    refactor(chain): Update implementation and add formattig support

--- chain.go
+++ chain.go
@@ -2,20 +2,35 @@ package errstack
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 )
 
 var _ ChainedError = (*ChainedStacktraceError)(nil)
 
-func NewChain(err error) *ChainedStacktraceError {
-	return newChainedStacktraceError(err)
+func NewChain(err error, opts ...StackErrOption) *ChainedStacktraceError {
+	return newChainedStacktraceError(err, opts...)
 }
 
-func newChainedStacktraceError(err error) *ChainedStacktraceError {
+func NewChainString(errStr string, opts ...StackErrOption) *ChainedStacktraceError {
+	return newChainedStacktraceErrorString(errStr, opts...)
+}
+
+func newChainedStacktraceError(err error, opts ...StackErrOption) *ChainedStacktraceError {
+	chainErr := &ChainedStacktraceError{}
+
+	if stErr, ok := err.(Error); ok {
+		chainErr.currErr = stErr
+	} else {
+		chainErr.currErr = New(err, opts...)
+	}
+
+	return chainErr
+}
+
+func newChainedStacktraceErrorString(errStr string, opts ...StackErrOption) *ChainedStacktraceError {
 	return &ChainedStacktraceError{
-		currErr: StacktraceError{
-			err: err,
-		},
+		currErr: NewString(errStr, opts...),
 	}
 }
 
@@ -25,38 +40,24 @@ func Chain(err1, err2 error) ChainedError {
 		return chErr1.Chain(err2)
 	}
 
-	currErr := &ChainedStacktraceError{
-		currErr: StacktraceError{
-			err: err1,
-		},
-	}
-
-	if btErr, ok := err2.(StackTracer); ok {
-		currErr.currErr.stackTrace = btErr.StackTrace()
-	}
-
-	return currErr.Chain(err2)
+	return newChainedStacktraceError(err1).Chain(err2)
 }
 
 type ChainedStacktraceError struct {
 	nextErr ChainedError
-	currErr StacktraceError
+	currErr Error
 }
 
 func (self *ChainedStacktraceError) Chain(err error) ChainedError {
+	if self == nil {
+		return nil
+	}
+
 	chErr, ok := err.(ChainedError)
 	if ok {
 		self.nextErr = chErr
 	} else {
-		nextErr := &ChainedStacktraceError{
-			currErr: StacktraceError{
-				err: err,
-			},
-		}
-		if btErr, ok := err.(StackTracer); ok {
-			nextErr.currErr.stackTrace = btErr.StackTrace()
-		}
-		self.nextErr = nextErr
+		self.nextErr = newChainedStacktraceError(err)
 	}
 
 	return self
@@ -89,40 +90,28 @@ func (self *ChainedStacktraceError) Next() ChainedError {
 	return self.nextErr
 }
 
+//go:noinline
 func (self *ChainedStacktraceError) Throw() ChainedError {
 	if self == nil {
 		return nil
 	}
 
-	frames := &self.currErr.stackTrace.Frames
-	*frames = append(*frames, caller(1))
+	self.currErr.Throw(1)
 
 	return self
 
 }
 
 func (self *ChainedStacktraceError) Error() string {
-	if self == nil {
-		return NilErrorString
-	}
-
-	sb := strings.Builder{}
-	chainElem := (ChainedError)(self)
-
-	for {
-		sb.WriteString(chainElem.Inner().Error())
-		chainElem = chainElem.Next()
-		if chainElem == nil {
-			break
-		}
-		sb.WriteString(ErrorChainSeparator)
-	}
-
-	return sb.String()
+	return fmt.Sprintf("%s", self)
 }
 
 func (self *ChainedStacktraceError) Inner() Error {
-	return &self.currErr
+	if self == nil {
+		return nil
+	}
+
+	return self.currErr
 }
 
 func (self *ChainedStacktraceError) MarshalJSON() ([]byte, error) {
@@ -131,13 +120,10 @@ func (self *ChainedStacktraceError) MarshalJSON() ([]byte, error) {
 	}
 
 	errList := ([]Error)(nil)
-	chainElem := (ChainedError)(self)
 
-	for {
-		errList = append(errList, chainElem.Inner())
-		chainElem = chainElem.Next()
-		if chainElem == nil {
-			break
+	for elem := (ChainedError)(self); elem != nil; elem = elem.Next() {
+		if elem.Inner() != nil {
+			errList = append(errList, elem.Inner())
 		}
 	}
 
@@ -161,4 +147,100 @@ func (self *ChainedStacktraceError) String() string {
 	}
 
 	return sb.String()
+}
+
+// Format formats the frame according to the fmt.Formatter interface. Format also accepts
+// flags that alter the printing of some verbs. The allowed combinations are as follows:
+//
+//	%s	Plain error string, no stack trace, separated by the separator configured in the
+//		default formatter
+//
+//	%+s	Same as %s, except the specified error formatter will be used for printing the string.
+//		The errors are separated by ": "
+//
+//	%v	Formatted error string, with just the function name in the stack trace.
+//
+//	% v	Same as %v, except the stack trace is printed on separate lines. This overrides the
+//		stack frame separator and the error separator to '\n'. Rest of the options are kept intact
+//
+//	%-v	Same as %v, except it will print the source location as well
+//
+//	%+v	Same as %-v, except it will print the stack trace on a separate line than the error. This
+//		overrides the stack frame separator and the error separator to '\n'. Rest of the options
+//		are kept intact. '+' can be followed by an arbitrary number which will represent the count
+//		of spaces used to indent the stack trace
+//
+//	%#v	Same as %+(n)v, except it will print stack indices as well
+//
+//	%j	Same as %-v, except it will be printed as a json string
+//
+//	%+j	Same as %j, except it will be pretty printed. '+' can be followed by an arbitrary number
+//		to indicate the indentation in the json output
+//
+// NOTE: Every verb defined above will always use the error and stack formatters defined in the package.
+// It will only override the options mentioned as part of the flags and the rest will be used as is. The user
+// is free to define other options of their choosing or provide entirely different implmentations as long as
+// the interfaces are satisfied.
+func (self *ChainedStacktraceError) Format(s fmt.State, verb rune) {
+	erFmt := DefaultChainErrorFormatter
+	stFmt := erFmt.StackTraceFormatter()
+	ffFmt := stFmt.FrameFormatter()
+
+	eOpts := erFmt.Options()
+	fOpts := ffFmt.Options()
+	sOpts := stFmt.Options()
+
+	switch verb {
+	case 's':
+		if s.Flag('+') {
+			eOpts.ErrorSeparator = ": "
+		}
+		eOpts.StackTraceSeparator = ""
+		erFmt.Clone().SetOptions(eOpts).FormatBuffer(s, self)
+
+	case 'v':
+		flags := byte(0)
+		switch {
+		case s.Flag(' '):
+			flags = flags | 1<<0
+		case s.Flag('-'):
+			flags = flags | 1<<1
+		case s.Flag('+'):
+			flags = flags | 1<<2
+		case s.Flag('#'):
+			flags = flags | 1<<3
+		default:
+		}
+
+		fOpts.SkipLocation = flags <= 1
+		sOpts.SkipStackIndex = flags&(1<<3) == 0
+		if flags&0x0d > 0 {
+			eOpts.ErrorSeparator = "\n"
+			eOpts.StackTraceSeparator = "\n"
+			sOpts.FrameSeparator = "\n"
+			if w, ok := s.Width(); ok {
+				w = max(2, w)
+				sOpts.FrameIndent = strings.Repeat(" ", w)
+			}
+		}
+
+		erFmt.Clone().
+			SetOptions(eOpts).
+			SetStackTraceFormatter(
+				stFmt.Clone().
+					SetOptions(sOpts).
+					SetFrameFormatter(
+						ffFmt.Clone().SetOptions(fOpts),
+					),
+			).FormatBuffer(s, self)
+
+	case 'j':
+		enc := json.NewEncoder(s)
+		if s.Flag('+') {
+			w, _ := s.Width()
+			enc.SetIndent("", strings.Repeat(" ", max(2, w)))
+		}
+		enc.Encode(self)
+	}
+
 }
--- chain_test.go
+++ chain_test.go
@@ -1,23 +1,219 @@
 package errstack
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_chainerr(t *testing.T) {
-	err := chainabc1()
-	t.Logf("\n%v", err)
-}
+func Test_ChainedErrorWithDefaults(t *testing.T) {
+	currDir, err := os.Getwd()
+	if err != nil {
+		t.Errorf("Failed to get current directory: error: %s", err)
+	}
 
-func chainabc1() ChainedError {
-	return Chain(errors.New("Error 1"), chainabc2())
-}
+	t.Run("automatic stacktrace collection", func(t *testing.T) {
+		chainabc3 := func() ChainedError {
+			return NewChain(NewString("Error 2", WithStack()))
+		}
+		chainabc2 := func() ChainedError {
+			return chainabc3()
+		}
+		chainabc1 := func() ChainedError {
+			return Chain(NewString("Error 1", WithStack()), chainabc2())
+		}
 
-func chainabc2() ChainedError {
-	return chainabc3().Throw()
-}
+		err := chainabc1()
 
-func chainabc3() ChainedError {
-	return NewChain(errors.New("Error 2")).Throw()
+		t.Run("printing formats", func(t *testing.T) {
+			type testCase struct {
+				format string
+				output strings.Builder
+				expect string
+			}
+
+			cases := []testCase{
+				{
+					format: "s",
+					expect: "Error 1, Error 2",
+				},
+				{
+					format: "+s",
+					expect: "Error 1: Error 2",
+				},
+				{
+					format: "v",
+					expect: "Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1;testing.tRunner;runtime.goexit, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1;testing.tRunner;runtime.goexit",
+				},
+				{
+					format: " v",
+					expect: "Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1\ntesting.tRunner\nruntime.goexit\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1\ntesting.tRunner\nruntime.goexit",
+				},
+				{
+					format: "-v",
+					expect: fmt.Sprintf("Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:28;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:31;testing.tRunner@/usr/local/go/src/testing/testing.go:1690;runtime.goexit@/usr/local/go/src/runtime/asm_amd64.s:1700, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:22;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:25;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:28;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:31;testing.tRunner@/usr/local/go/src/testing/testing.go:1690;runtime.goexit@/usr/local/go/src/runtime/asm_amd64.s:1700", currDir, currDir, currDir, currDir, currDir, currDir),
+				},
+				{
+					format: "+v",
+					expect: fmt.Sprintf("Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:28\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:31\ntesting.tRunner@/usr/local/go/src/testing/testing.go:1690\nruntime.goexit@/usr/local/go/src/runtime/asm_amd64.s:1700\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:22\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:25\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:28\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:31\ntesting.tRunner@/usr/local/go/src/testing/testing.go:1690\nruntime.goexit@/usr/local/go/src/runtime/asm_amd64.s:1700", currDir, currDir, currDir, currDir, currDir, currDir),
+				},
+				{
+					format: "#v",
+					expect: fmt.Sprintf("Error 1\n#3: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:28\n#2: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:31\n#1: testing.tRunner@/usr/local/go/src/testing/testing.go:1690\n#0: runtime.goexit@/usr/local/go/src/runtime/asm_amd64.s:1700\nError 2\n#5: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:22\n#4: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:25\n#3: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:28\n#2: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:31\n#1: testing.tRunner@/usr/local/go/src/testing/testing.go:1690\n#0: runtime.goexit@/usr/local/go/src/runtime/asm_amd64.s:1700", currDir, currDir, currDir, currDir, currDir, currDir),
+				},
+				{
+					format: "j",
+					expect: func() string {
+						errList := []Error{}
+						for chErr := err; chErr != nil; chErr = chErr.Next() {
+							errList = append(errList, chErr.Inner())
+						}
+						b, _ := json.Marshal(errList)
+						return string(b) + "\n"
+					}(),
+				},
+				{
+					format: "+j",
+					expect: func() string {
+						errList := []Error{}
+						for chErr := err; chErr != nil; chErr = chErr.Next() {
+							errList = append(errList, chErr.Inner())
+						}
+						b, _ := json.MarshalIndent(errList, "", "  ")
+						return string(b) + "\n"
+					}(),
+				},
+				{
+					format: "+4j",
+					expect: func() string {
+						errList := []Error{}
+						for chErr := err; chErr != nil; chErr = chErr.Next() {
+							errList = append(errList, chErr.Inner())
+						}
+						b, _ := json.MarshalIndent(errList, "", "    ")
+						return string(b) + "\n"
+					}(),
+				},
+			}
+
+			for _, c := range cases {
+				t.Run(c.format, func(t *testing.T) {
+					fmt.Fprintf(&c.output, "%"+c.format, err)
+					if c.output.String() != c.expect {
+						assert.Equal(
+							t,
+							c.expect,
+							c.output.String(),
+							"Doesn't match the expected output",
+						)
+					}
+				})
+			}
+		})
+	})
+
+	t.Run("manual stacktrace collection", func(t *testing.T) {
+		chainabc3 := func() ChainedError {
+			return NewChain(errors.New("Error 2")).Throw()
+		}
+		chainabc2 := func() ChainedError {
+			return chainabc3().Throw()
+		}
+		chainabc1 := func() ChainedError {
+			return Chain(NewString("Error 1"), chainabc2()).Throw()
+		}
+
+		err := chainabc1().Throw()
+
+		t.Run("printing formats", func(t *testing.T) {
+			type testCase struct {
+				format string
+				output strings.Builder
+				expect string
+			}
+
+			cases := []testCase{
+				{
+					format: "s",
+					expect: "Error 1, Error 2",
+				},
+				{
+					format: "+s",
+					expect: "Error 1: Error 2",
+				},
+				{
+					format: "v",
+					expect: "Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2",
+				},
+				{
+					format: " v",
+					expect: "Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2",
+				},
+				{
+					format: "-v",
+					expect: fmt.Sprintf("Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:128;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:131, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:122;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:125", currDir, currDir, currDir, currDir),
+				},
+				{
+					format: "+v",
+					expect: fmt.Sprintf("Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:128\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:131\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:122\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:125", currDir, currDir, currDir, currDir),
+				},
+				{
+					format: "#v",
+					expect: fmt.Sprintf("Error 1\n#1: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:128\n#0: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:131\nError 2\n#1: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:122\n#0: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:125", currDir, currDir, currDir, currDir),
+				},
+				{
+					format: "j",
+					expect: func() string {
+						errList := []Error{}
+						for chErr := err; chErr != nil; chErr = chErr.Next() {
+							errList = append(errList, chErr.Inner())
+						}
+						b, _ := json.Marshal(errList)
+						return string(b) + "\n"
+					}(),
+				},
+				{
+					format: "+j",
+					expect: func() string {
+						errList := []Error{}
+						for chErr := err; chErr != nil; chErr = chErr.Next() {
+							errList = append(errList, chErr.Inner())
+						}
+						b, _ := json.MarshalIndent(errList, "", "  ")
+						return string(b) + "\n"
+					}(),
+				},
+				{
+					format: "+4j",
+					expect: func() string {
+						errList := []Error{}
+						for chErr := err; chErr != nil; chErr = chErr.Next() {
+							errList = append(errList, chErr.Inner())
+						}
+						b, _ := json.MarshalIndent(errList, "", "    ")
+						return string(b) + "\n"
+					}(),
+				},
+			}
+
+			for _, c := range cases {
+				t.Run(c.format, func(t *testing.T) {
+					fmt.Fprintf(&c.output, "%"+c.format, err)
+					if c.output.String() != c.expect {
+						assert.Equal(
+							t,
+							c.expect,
+							c.output.String(),
+							"Doesn't match the expected output",
+						)
+					}
+				})
+			}
+		})
+	})
 }
