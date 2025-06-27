21f7935a8f3e25d813cf61b706ea1c5cc30d79c8
nnishant776 <nn54565852@gmail.com>
2025-06-27 16:31:59 +0000

    test(chain): Update UT

--- chain_test.go
+++ chain_test.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"runtime"
 	"strings"
 	"testing"
 
+	pkgerrs "github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,15 +60,15 @@ func Test_ChainedErrorWithDefaults(t *testing.T) {
 				},
 				{
 					format: "-v",
-					expect: fmt.Sprintf("Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:30;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:33;testing.tRunner@%s/src/testing/testing.go:1690;runtime.goexit@%s/src/runtime/asm_amd64.s:1700, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:24;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:27;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:30;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:33;testing.tRunner@%s/src/testing/testing.go:1690;runtime.goexit@%s/src/runtime/asm_amd64.s:1700", currDir, currDir, goRoot, goRoot, currDir, currDir, currDir, currDir, goRoot, goRoot),
+					expect: fmt.Sprintf("Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:32;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:35;testing.tRunner@%s/src/testing/testing.go:1690;runtime.goexit@%s/src/runtime/asm_amd64.s:1700, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:26;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:29;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:32;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:35;testing.tRunner@%s/src/testing/testing.go:1690;runtime.goexit@%s/src/runtime/asm_amd64.s:1700", currDir, currDir, goRoot, goRoot, currDir, currDir, currDir, currDir, goRoot, goRoot),
 				},
 				{
 					format: "+v",
-					expect: fmt.Sprintf("Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:30\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:33\ntesting.tRunner@%s/src/testing/testing.go:1690\nruntime.goexit@%s/src/runtime/asm_amd64.s:1700\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:24\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:27\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:30\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:33\ntesting.tRunner@%s/src/testing/testing.go:1690\nruntime.goexit@%s/src/runtime/asm_amd64.s:1700", currDir, currDir, goRoot, goRoot, currDir, currDir, currDir, currDir, goRoot, goRoot),
+					expect: fmt.Sprintf("Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:32\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:35\ntesting.tRunner@%s/src/testing/testing.go:1690\nruntime.goexit@%s/src/runtime/asm_amd64.s:1700\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:26\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:29\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:32\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:35\ntesting.tRunner@%s/src/testing/testing.go:1690\nruntime.goexit@%s/src/runtime/asm_amd64.s:1700", currDir, currDir, goRoot, goRoot, currDir, currDir, currDir, currDir, goRoot, goRoot),
 				},
 				{
 					format: "#v",
-					expect: fmt.Sprintf("Error 1\n#3: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:30\n#2: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:33\n#1: testing.tRunner@%s/src/testing/testing.go:1690\n#0: runtime.goexit@%s/src/runtime/asm_amd64.s:1700\nError 2\n#5: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:24\n#4: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:27\n#3: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:30\n#2: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:33\n#1: testing.tRunner@%s/src/testing/testing.go:1690\n#0: runtime.goexit@%s/src/runtime/asm_amd64.s:1700", currDir, currDir, goRoot, goRoot, currDir, currDir, currDir, currDir, goRoot, goRoot),
+					expect: fmt.Sprintf("Error 1\n#3: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:32\n#2: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:35\n#1: testing.tRunner@%s/src/testing/testing.go:1690\n#0: runtime.goexit@%s/src/runtime/asm_amd64.s:1700\nError 2\n#5: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.1@%s/chain_test.go:26\n#4: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.2@%s/chain_test.go:29\n#3: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1.3@%s/chain_test.go:32\n#2: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func1@%s/chain_test.go:35\n#1: testing.tRunner@%s/src/testing/testing.go:1690\n#0: runtime.goexit@%s/src/runtime/asm_amd64.s:1700", currDir, currDir, goRoot, goRoot, currDir, currDir, currDir, currDir, goRoot, goRoot),
 				},
 				{
 					format: "j",
@@ -158,15 +160,15 @@ func Test_ChainedErrorWithDefaults(t *testing.T) {
 				},
 				{
 					format: "-v",
-					expect: fmt.Sprintf("Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:130;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:133, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:124;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:127", currDir, currDir, currDir, currDir),
+					expect: fmt.Sprintf("Error 1=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:132;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:135, Error 2=>github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:126;github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:129", currDir, currDir, currDir, currDir),
 				},
 				{
 					format: "+v",
-					expect: fmt.Sprintf("Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:130\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:133\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:124\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:127", currDir, currDir, currDir, currDir),
+					expect: fmt.Sprintf("Error 1\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:132\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:135\nError 2\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:126\ngithub.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:129", currDir, currDir, currDir, currDir),
 				},
 				{
 					format: "#v",
-					expect: fmt.Sprintf("Error 1\n#1: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:130\n#0: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:133\nError 2\n#1: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:124\n#0: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:127", currDir, currDir, currDir, currDir),
+					expect: fmt.Sprintf("Error 1\n#1: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.3@%s/chain_test.go:132\n#0: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2@%s/chain_test.go:135\nError 2\n#1: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.1@%s/chain_test.go:126\n#0: github.com/nnishant776/errstack.Test_ChainedErrorWithDefaults.func2.2@%s/chain_test.go:129", currDir, currDir, currDir, currDir),
 				},
 				{
 					format: "j",
@@ -215,6 +217,68 @@ func Test_ChainedErrorWithDefaults(t *testing.T) {
 						)
 					}
 				})
+			}
+		})
+	})
+}
+
+func Benchmark_ChainedStackTraceError(b *testing.B) {
+	type ifce interface {
+		StackTrace() pkgerrs.StackTrace
+	}
+
+	b.Run("pkgerrrs", func(b *testing.B) {
+		b.Run("print error string only", func(b *testing.B) {
+			b.Run("no stack capture", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					err := pkgerrs.New("pkgerrs")
+					err = pkgerrs.Wrap(err, "wrapped pkgerrs")
+					fmt.Fprintf(io.Discard, "%s", err)
+				}
+			})
+
+			b.Run("with stack capture", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					err := pkgerrs.WithStack(pkgerrs.New("pkgerrs"))
+					err = pkgerrs.Wrap(err, "wrapped pkgerrs")
+					fmt.Fprintf(io.Discard, "%s", err)
+				}
+			})
+		})
+
+		b.Run("print error and stacktrace", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				err := pkgerrs.WithStack(pkgerrs.New("pkgerrs"))
+				err = pkgerrs.Wrap(err, "wrapped pkgerrs")
+				fmt.Fprintf(io.Discard, "%+v", err)
+			}
+		})
+	})
+
+	b.Run("errstack", func(b *testing.B) {
+		b.Run("print error string only", func(b *testing.B) {
+			b.Run("no stack capture", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					err := NewString("errstk")
+					errw := NewChainString("wrapped errstk", WithStack()).Chain(err)
+					fmt.Fprintf(io.Discard, "%s", errw)
+				}
+			})
+
+			b.Run("with stack capture", func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					err := NewString("errstk", WithStack())
+					errw := NewChainString("wrapped errstk", WithStack()).Chain(err)
+					fmt.Fprintf(io.Discard, "%s", errw)
+				}
+			})
+		})
+
+		b.Run("print error and stacktrace", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				err := NewString("errstk", WithStack())
+				errw := NewChainString("wrapped errstk", WithStack()).Chain(err)
+				fmt.Fprintf(io.Discard, "%-v", errw)
 			}
 		})
 	})
